833207964fa1f0dea2840148ee8bc59268a7b059
Augusto Balbin <abalbinr@unal.edu.co>
2021-02-09 18:10:54 -0500

    Verificación de Código Estático con eslint

--- test/MyFirstApiConsume.test.js
+++ test/MyFirstApiConsume.test.js
@@ -4,31 +4,31 @@ const agent = require('superagent'); // Documentación https://visionmedia.githu
 const statusCode = require('http-status-codes');
 const chai = require('chai');
 
-const expect = chai.expect;
+const { expect } = chai;
 
 describe('First Api Tests', () => {
 });
 
 // prueba consumiendo un servicio GET
 it('Consume GET Service', async () => {
-    const response = await agent.get('https://httpbin.org/ip');
-  
-    expect(response.status).to.equal(statusCode.OK);
-    expect(response.body).to.have.property('origin');
+  const response = await agent.get('https://httpbin.org/ip');
+
+  expect(response.status).to.equal(statusCode.OK);
+  expect(response.body).to.have.property('origin');
 });
 
 // prueba consumiendo un servicio GET con Query Parameters
 it('Consume GET Service with query parameters', async () => {
-    const query = {
-      name: 'John',
-      age: '31',
-      city: 'New York'
-    };
-  
-    const response = await agent.get('https://httpbin.org/get').query(query);
-  
-    expect(response.status).to.equal(statusCode.OK);
-    expect(response.body.args).to.eql(query);
+  const query = {
+    name: 'John',
+    age: '31',
+    city: 'New York'
+  };
+
+  const response = await agent.get('https://httpbin.org/get').query(query);
+
+  expect(response.status).to.equal(statusCode.OK);
+  expect(response.body.args).to.eql(query);
 });
 
 //
