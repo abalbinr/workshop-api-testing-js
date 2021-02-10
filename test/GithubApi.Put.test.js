dd49ad8b7be117c949a2459f85e9f4843acc0e8d
Augusto Balbin <abalbinr@unal.edu.co>
2021-02-10 16:36:04 -0500

    Se volvió a seguir a aperdomob

--- test/GithubApi.Put.test.js
+++ test/GithubApi.Put.test.js
@@ -22,4 +22,12 @@ describe('Consumiendo Métodos PUT', () => {
     usuario = response.body.find((usuarios) => usuarios.login === 'aperdomob'); // Buscar de mis seguidores a aperdomob
     expect(usuario.login).equal('aperdomob');
   });
+
+  it('Volver a seguir al usuario aperdomob', async () => {
+    const response = await agent.put(`${urlBase}/aperdomob`)
+      .auth('token', 'dcef4d17bb0d1746f91a9d355da1936b06f56cc7')
+      .set('User-Agent', 'agent');
+
+    expect(response.status).to.equal(statusCode.NO_CONTENT); // 204 No Content
+  });
 });
