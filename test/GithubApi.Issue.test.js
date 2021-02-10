7b2a273f184d637607e3fd30bc2abef95bc27e82
Augusto Balbin <abalbinr@unal.edu.co>
2021-02-10 18:53:39 -0500

    Verificar que exista un repo de la url de repos del usuario

--- test/GithubApi.Issue.test.js
+++ test/GithubApi.Issue.test.js
@@ -2,6 +2,7 @@ const agent = require('superagent');
 const { expect } = require('chai');
 
 const urlBase = 'https://api.github.com/user';
+let usuario;
 
 describe('Consumiendo métodos POST y PATCH', () => {
   it('Verificar si este usuario tiene repositorios publicos', async () => {
@@ -11,5 +12,17 @@ describe('Consumiendo métodos POST y PATCH', () => {
 
     const repoPublico = response.body.public_repos > 0;
     expect(repoPublico).to.equal(true); // https://www.chaijs.com/api/bdd/
+    usuario = response.body;
+  });
+
+  it('la lista de todos los repositorios de este usuario', async () => {
+    const response = await agent.get(`${usuario.repos_url}`)
+      .auth('token', 'dcef4d17bb0d1746f91a9d355da1936b06f56cc7')
+      .set('User-Agent', 'agent');
+
+    const repoID = response.body[0].id;
+
+    const repositorio = response.body.find((repos) => repos.name === 'Kaggle');
+    expect(repositorio.id).to.equal(repoID);
   });
 });
